f0261421b591b1dae04f0b49c5694aaf4f15a6c1
sergeymasl <83965287+sergeymasl@users.noreply.github.com>
2022-06-27 20:39:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,33 +6,16 @@ from db import database
 
 
 async def on_startup(dispatcher):
-    await database.connect()
     await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
 
 
 async def on_shutdown(dispatcher):
-    await database.disconnect()
     await bot.delete_webhook()
-
-
-async def save(user_id, text):
-    await database.execute(f"INSERT INTO messages(telegram_id, text) "
-                           f"VALUES (:telegram_id, :text)", values={'telegram_id': user_id, 'text': text})
-
-
-async def read(user_id):
-    results = await database.fetch_all('SELECT text '
-                                       'FROM messages '
-                                       'WHERE telegram_id = :telegram_id ',
-                                       values={'telegram_id': user_id})
-    return [next(result.values()) for result in results]
 
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    await save(message.from_user.id, message.text)
-    messages = await read(message.from_user.id)
-    await message.answer(messages)
+    await message.answer('Принято')
 
 
 if __name__ == '__main__':
