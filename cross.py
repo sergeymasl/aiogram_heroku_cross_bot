60b48ca41e72284822d66665da83be54fc70f3df
sergeymasl <83965287+sergeymasl@users.noreply.github.com>
2022-06-27 20:24:58 +0300

    Update cross.py

--- cross.py
+++ cross.py
@@ -1,10 +1,6 @@
-import logging
-import os
-from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
-from aiogram.utils.executor import start_webhook
-from aiogram import Bot, types
-
+from aiogram import Bot
+import os
 
 TOKEN = os.getenv('BOT_TOKEN')
 bot = Bot(token=TOKEN)
@@ -20,29 +16,4 @@ WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 # webserver settings
 WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = os.getenv('PORT', default=8000)
-
-
-async def on_startup(dispatcher):
-    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
-
-
-async def on_shutdown(dispatcher):
-    await bot.delete_webhook()
-
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(message.text)
-
-
- if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        skip_updates=True,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
+DB_URL = os.getenv('DATABASE_URL')
